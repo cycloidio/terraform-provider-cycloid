3aa3923a5e97491c5dfe8605d06de058424d8a94
Łukasz Opasiak <lukasz.opasiak@proton.me>
2025-12-18 15:32:14 +0100

    prepare fixes for onboarding and stack versioning features
    This doesn't include stack versioning support
    
    # Conflicts:
    #	Makefile
    #	go.mod
    #	go.sum
    #	provider/config_repository_resource.go
    #	provider/organization_resource.go

--- provider/utils.go
+++ provider/utils.go
@@ -2,11 +2,21 @@ package provider
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/cycloidio/cycloid-cli/cmd/cycloid/common"
 	"github.com/cycloidio/terraform-provider-cycloid/provider_cycloid"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
+
+// normalizeURL ensures the URL has a valid protocol scheme (http:// or https://).
+// If no scheme is provided, it defaults to http://.
+func normalizeURL(url string) string {
+	if strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://") {
+		return url
+	}
+	return "http://" + url
+}
 
 func getOrganizationCanonical(pv provider_cycloid.CycloidModel, dataOrgCan types.String) string {
 	orgCan := pv.OrganizationCanonical.ValueString()
