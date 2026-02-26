e5bbf0756edc313823196239391254a18edc1801
Frédéric BARRAS HAMPEL <frederic.hampel@cycloid.io>
2026-02-26 01:29:40 +0100

    func: add project and environment resources

---
+++ provider/project_resource.go
@@ -0,0 +1,252 @@
+package provider
+
+import (
+	"context"
+	"slices"
+
+	"github.com/cycloidio/cycloid-cli/client/models"
+	"github.com/cycloidio/terraform-provider-cycloid/internal/icons"
+	"github.com/cycloidio/terraform-provider-cycloid/internal/ptr"
+	"github.com/cycloidio/terraform-provider-cycloid/resource_project"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/sanity-io/litter"
+)
+
+var _ resource.Resource = (*projectResource)(nil)
+
+func NewProjectResource() resource.Resource {
+	return &projectResource{}
+}
+
+type projectResourceModel resource_project.ProjectModel
+
+type projectResource struct {
+	provider CycloidProvider
+}
+
+func (p *projectResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_project"
+}
+
+func (p *projectResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = resource_project.ProjectResourceSchema(ctx)
+}
+
+func (p *projectResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	pv, ok := req.ProviderData.(*CycloidProvider)
+	if !ok {
+		resp.Diagnostics.AddError("missing provider data", litter.Sdump(req))
+		return
+	}
+
+	p.provider = *pv
+}
+
+func (p *projectResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data projectResourceModel
+
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	m := p.provider.Middleware
+	canonical := data.Canonical.ValueString()
+
+	org := getOrganizationCanonical(p.provider, data.Organization)
+	projects, err := m.ListProjects(org)
+	if err != nil {
+		resp.Diagnostics.AddError("failed to fetch project from API", err.Error())
+		return
+	}
+
+	if i := slices.IndexFunc(projects, func(p *models.Project) bool {
+		return ptr.Value(p.Canonical) == canonical
+	}); i == -1 {
+		// Project doesn't exist, so empty state
+		resp.Diagnostics.Append(projectToValue(ctx, org, &models.Project{}, &data)...)
+		if resp.Diagnostics.HasError() {
+			return
+		}
+	} else {
+		resp.Diagnostics.Append(projectToValue(ctx, org, projects[i], &data)...)
+		if resp.Diagnostics.HasError() {
+			return
+		}
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (p *projectResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data projectResourceModel
+
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	name := data.Name.ValueString()
+	canonical := data.Canonical.ValueString()
+	org := getOrganizationCanonical(p.provider, data.Organization)
+	description := data.Description.ValueString()
+	color := data.Color.ValueString()
+	if color == "" {
+		color = icons.RandomColor()
+	}
+
+	icon := data.Icon.ValueString()
+	if icon == "" {
+		icon = icons.RandomIcon()
+	}
+	owner := data.Owner.ValueString()
+	configRepository := data.ConfigRepository.ValueString()
+
+	data, d := p.createOrUpdateProject(ctx, org, name, canonical, description, configRepository, owner, owner, color, icon, false)
+	resp.Diagnostics.Append(d...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (p *projectResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data projectResourceModel
+
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	name := data.Name.ValueString()
+	canonical := data.Canonical.ValueString()
+	org := getOrganizationCanonical(p.provider, data.Organization)
+	description := data.Description.ValueString()
+	color := data.Color.ValueString()
+	if color == "" {
+		color = icons.RandomIcon()
+	}
+
+	icon := data.Icon.ValueString()
+	if icon == "" {
+		icon = icons.RandomIcon()
+	}
+	owner := data.Owner.ValueString()
+	configRepository := data.ConfigRepository.ValueString()
+
+	data, d := p.createOrUpdateProject(ctx, org, name, canonical, description, configRepository, owner, owner, color, icon, true)
+	resp.Diagnostics.Append(d...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (p *projectResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data projectResourceModel
+
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	m := p.provider.Middleware
+	org := getOrganizationCanonical(p.provider, data.Organization)
+	canonical := data.Canonical.ValueString()
+
+	err := m.DeleteProject(org, canonical)
+	if err != nil {
+		resp.Diagnostics.AddError("failed to fetch project from API", err.Error())
+		return
+	}
+
+	resp.Diagnostics.Append(projectToValue(ctx, org, &models.Project{}, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func projectToValue(ctx context.Context, org string, project *models.Project, data *projectResourceModel) diag.Diagnostics {
+	if project == nil {
+		return diag.Diagnostics{diag.NewErrorDiagnostic("project is nil for convertion", "This should not happend, please contact the plugin maintainer.")}
+	}
+	data.Canonical = types.StringPointerValue(project.Canonical)
+	data.Name = types.StringPointerValue(project.Name)
+	data.Organization = types.StringValue(org)
+	data.Description = types.StringValue(project.Description)
+	data.Color = types.StringPointerValue(project.Color)
+	data.Icon = types.StringPointerValue(project.Icon)
+	data.Owner = types.StringPointerValue(ptr.Value(project.Owner).Username)
+	data.ConfigRepository = types.StringValue(project.ConfigRepositoryCanonical)
+	return nil
+}
+
+func (p *projectResource) createOrUpdateProject(ctx context.Context, org, name, canonical, description, configRepository, owner, team, color, icon string, isUpdate bool) (projectResourceModel, diag.Diagnostics) {
+	var diags diag.Diagnostics
+	var data projectResourceModel
+	var err error
+
+	name, canonical, err = NameOrCanonical(name, canonical)
+	if err != nil {
+		diags.AddError("failed to infer canonical", err.Error())
+		return data, diags
+	}
+
+	if configRepository == "" {
+		configRepositories, err := p.provider.Middleware.ListConfigRepositories(org)
+		if err != nil {
+			diags.AddError("failed to fetch list of current config repositories to infer default catalog", err.Error())
+			return data, diags
+		}
+
+		if i := slices.IndexFunc(configRepositories, func(c *models.ConfigRepository) bool {
+			return ptr.Value(c.Default)
+		}); i != -1 {
+			configRepository = ptr.Value(configRepositories[i].Canonical)
+		} else {
+			diags.AddError("no default catalog repository was found", "please add a default config repository to the org "+org+" or fill the `catalog_respository` attribute. Docs: https://docs.cycloid.io/reference/config-and-catalog-repository/")
+			return data, diags
+		}
+	}
+
+	projects, err := p.provider.Middleware.ListProjects(org)
+	if err != nil {
+		diags.AddError("failed to fetch projects from API", err.Error())
+		return data, diags
+	}
+
+	var project *models.Project
+	if i := slices.IndexFunc(projects, func(p *models.Project) bool {
+		return ptr.Value(p.Canonical) == canonical
+	}); i == -1 {
+		if isUpdate {
+			tflog.Info(ctx, "did not found current project, assuming it had been deleted outside the provider, re-creating...", nil)
+		}
+
+		project, err = p.provider.Middleware.CreateProject(org, name, canonical, description, configRepository, owner, owner, color, icon)
+		if err != nil {
+			diags.AddError("failed to create project from API", err.Error())
+			return data, diags
+		}
+	} else {
+		project, err = p.provider.Middleware.UpdateProject(org, name, canonical, description, configRepository, owner, owner, color, icon, "")
+		if err != nil {
+			diags.AddError("failed to update project from API", err.Error())
+			return data, diags
+		}
+	}
+
+	diags.Append(projectToValue(ctx, org, project, &data)...)
+	return data, diags
+}
--- provider/utils.go
+++ provider/utils.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"regexp"
 	"strings"
 
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -15,10 +16,40 @@ func normalizeURL(url string) string {
 	return "http://" + url
 }
 
+var allowedCanonicalCharRegex = regexp.MustCompile("[^a-zA-Z0-9-_]")
+
 func getOrganizationCanonical(provider CycloidProvider, dataOrgCan types.String) string {
 	if org := dataOrgCan.ValueString(); org != "" {
 		return org
 	}
 
 	return provider.DefaultOrganization
+}
+
+// ToCanonical convert a name to a valid canonical
+func ToCanonical(name string) string {
+	replacer := strings.NewReplacer(
+		" ", "_",
+	)
+
+	replaced := replacer.Replace(name)
+	filtered := allowedCanonicalCharRegex.ReplaceAllString(replaced, "")
+	trimmed := strings.Trim(filtered, "-_")
+	return strings.ToLower(trimmed)
+}
+
+// NameOrCanonical will process name and canonical argument and return both
+// if name is set, the canonical will be inferred from it
+// if canonical is set, name will be a Capitalized version of the canonical
+// if both are empty, you will get an error
+func NameOrCanonical(name, canonical string) (string, string, error) {
+	if canonical == "" {
+		return name, ToCanonical(name), nil
+	}
+
+	if name == "" {
+		return strings.ToUpper(canonical[:1]) + canonical[1:], canonical, nil
+	}
+
+	return name, canonical, nil
 }
